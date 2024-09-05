503e39a40070180f474adf4d4bce4c5cbc368c8e
Sonu Kumar <sonu82256@gmail.com>
2024-09-05 22:58:27 +0530

    deploy setup

--- api/index.js
+++ api/index.js
@@ -5,10 +5,12 @@ import taskRoutes from './routues/task.routes.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import dotenv from 'dotenv';
-
+import path from 'path';
 
 const app = express();
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5173',  // allow requests from this origin
@@ -32,6 +34,12 @@ app.use(cookieParser());
 
 app.use('/api/auth', userRoutes);
 app.use('/api/task', taskRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+ })
 
 app.listen(process.env.PORT || 4001, ()=>{
     console.log(`Server is running on port ${process.env.PORT }`)
